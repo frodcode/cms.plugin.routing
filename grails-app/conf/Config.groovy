c54deec931dd9d4ff9f15e3cf381768ee2159f2a
Frantisek Odehnal <odehnal@medio.cz>
2013-01-06 12:59:22 +0100

    Added spring security plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -5,33 +5,39 @@ import domain.routing.Page
 grails.gorm.failOnError = true
 
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+	// Example of changing the log pattern for the default console
+	// appender:
+	//
+	//appenders {
+	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+	//}
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+	error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+			'org.codehaus.groovy.grails.web.pages', //  GSP
+			'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+			'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+			'org.codehaus.groovy.grails.web.mapping', // URL mapping
+			'org.codehaus.groovy.grails.commons', // core / classloading
+			'org.codehaus.groovy.grails.plugins', // plugins
+			'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+			'org.springframework',
+			'org.hibernate',
+			'net.sf.ehcache.hibernate'
 
-    warn   'org.mortbay.log'
+	warn   'org.mortbay.log'
 }
 
 environments {
 	testPlugin {
 		//-Dgrails.env=testPlugin
-		def page = new Page();		
+		def page = new Page();
 		setting = "bar"
 	}
- }grails.views.default.codec="none" // none, html, base64
+}
+grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'routing.domain.auth.AuthUser'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'routing.domain.auth.AuthUserAuthRole'
+grails.plugins.springsecurity.authority.className = 'routing.domain.auth.AuthRole'
